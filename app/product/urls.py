4436e8a111a3c1ea06949e1cc819104ccb99489b
大友 良介 <r.otomo@miracleave.co.jp>
2021-09-02 21:11:44 +0900

    0901の内部リハ指摘事項取り込み

--- app/product/urls.py
+++ app/product/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from app.product.views.product_create_view import CreateProductView
+from app.product.views.product_update_view import ProductUpdateView
 from app.product.views.product_search_view import ProductSearchListView
 from django.views.generic import TemplateView
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='product/product_top.html'), name='top'),
     path('search/', ProductSearchListView.as_view(), name='product_search'),
     path('create/', CreateProductView.as_view(), name='product_create'),
+    path('update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
 ]
